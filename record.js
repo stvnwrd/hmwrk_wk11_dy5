7039925926bd554ebd51f429eea9daffa9ec1d34
Steven Ward <34653526+stvnwrd@users.noreply.github.com>
2018-04-07 14:32:58 +0100

    Initial Commit - Setup and basic record object created

---
+++ record.js
@@ -0,0 +1,8 @@
+var Record = function(params){
+  this.artist = params.artist;
+  this.title = params.title;
+  this.genre = params.genre;
+  this.price = params.price;
+};
+
+module.exports = Record;
---
+++ specs/record_spec.js
@@ -0,0 +1,32 @@
+var assert = require("assert");
+var Record = require("../record");
+
+describe("Record", function(){
+
+  var record
+
+  beforeEach(function(){
+    record1 = new Record({artist: "Lanark Artefax", title: "Whities 011", genre: "Electronic", price: 10});
+  });
+
+  it("should have an artist", function(){
+    assert.strictEqual(record1.artist, "Lanark Artefax")
+  });
+
+  it("should have a title", function(){
+    assert.strictEqual(record1.title, "Whities 011")
+  });
+
+  it("should have a genre", function(){
+    assert.strictEqual(record1.genre, "Electronic")
+  });
+
+  it("should have a price", function(){
+    assert.strictEqual(record1.price, 10)
+  });
+
+  
+
+
+
+})
